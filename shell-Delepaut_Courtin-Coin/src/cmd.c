d36e416d537f19039a428c65276fd433b58a5311
ChairHomard45 <147397853+ChairHomard45@users.noreply.github.com>
2023-10-17 15:49:04 +0200

    Update cmd.c

--- shell-Delepaut_Courtin-Coin/src/cmd.c
+++ shell-Delepaut_Courtin-Coin/src/cmd.c
@@ -18,7 +18,22 @@ int exec_cmd(cmd_t* p) {
 }
 
 int init_cmd(cmd_t* p) {
-
+  p->pid = -1; //PID Valeur Défaut
+  p->status = 0;
+  p->stdin = 0; //stdin Valeur Défaut
+  p->stdout = 1; //stdout Valeur Défaut
+  p->stderr = 2; //stderr Valeur Défaut
+  p->wait = 1; //wait Valeur Défaut
+  for (int j=0; j<MAX_CMD_SIZE; j++) {
+    if (p->fdclose[j] != -1){
+      close(p->fdclose[j]);
+      p->fdclose[j] = -1;
+    }
+  }
+  p->next=NULL;
+  p->next_success=NULL;
+  p->next_failure=NULL;
+  return 0;
 }
 
 int parse_cmd(char* tokens[], cmd_t* cmds, size_t max) {
