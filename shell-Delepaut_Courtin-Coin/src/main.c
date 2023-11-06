e8dd8823d02e39d8cfa79a791231668143d6b85a
ChairHomard45 <147397853+ChairHomard45@users.noreply.github.com>
2023-11-06 21:47:44 +0100

    Update main.c

--- shell-Delepaut_Courtin-Coin/src/main.c
+++ shell-Delepaut_Courtin-Coin/src/main.c
@@ -18,7 +18,6 @@
 
 #include "parser.h"
 #include "cmd.h"
-//#include "builtin.h"
 
 int main(int argc, char* argv[]) {
   char cmdline[MAX_LINE_SIZE]; // buffer des lignes de commandes
@@ -29,7 +28,6 @@ int main(int argc, char* argv[]) {
 
   //Chemin fichier
   char cwd[MAX_LINE_SIZE];
-  char* home_cwd = NULL;
 
   //initialise tous les tableau de pointeurs à NULL
   for (int i=0; i<MAX_CMD_SIZE; i++) {
@@ -38,15 +36,9 @@ int main(int argc, char* argv[]) {
     for(int j=0; j<MAX_CMD_SIZE; j++)
       cmds[i].argv[j]=NULL;
   }
-
-  //for (int i = 0; i < MAX_CMD_SIZE; i++) {init_cmd(&cmds[i]);}
-  init_cmd(&cmds[0]);
-
-  //Récupère les variables pour HOME / USER / COMPUTER
-  char* home = getenv("HOME");
-  if (home == NULL) {
-    home = "C:\\Users\\delep";  // Provide a default value
-  }
+  for (int i = 0; i < MAX_CMD_SIZE; i++) {init_cmd(&cmds[i]);}
+  current=NULL;
+  //Récupère les variables pour USER 
   char* user = getenv("USER");
   if (user == NULL) {
     // Handle the case where the "USER" environment variable is not set
@@ -54,14 +46,13 @@ int main(int argc, char* argv[]) {
 }
 
   while (1) {
-    printf("Debut\n");
+    printf("\nDebut\n");
 
     // Effacer les contenus de cmdline, cmdtoks et cmds
-    memset(cmdline, 0, sizeof(cmdline));
+    cmdline[0] = '\0';
     for(int i=0;i<MAX_CMD_SIZE;i++) {
       if(cmdtoks[i]!=NULL){
-        free(cmdtoks[i]);
-        cmdtoks[i] = NULL;
+        cmdtoks[i] = '\0';
       }
     }
 
@@ -70,28 +61,23 @@ int main(int argc, char* argv[]) {
 
     //Initialise home_cwd
     getcwd(cwd, MAX_LINE_SIZE-1);
-    size_t len = strlen(home) + strlen(cwd) + 1;
-    home_cwd = (char*)malloc(len);
-    if (home_cwd != NULL) {
-      strcpy(home_cwd, home);
-      strcat(home_cwd, "/");
-      strcat(home_cwd, cwd + strlen(home));
-    } else {
-      printf("Memory allocation failed\n");
-    }
 
     // Afficher un prompt et affiche [USER] [COMPUTER] [ENV] [DIRECTORY] et les met en couleur
-    printf("\n\033[32m%s\033[0m \033[35mMINISHELL-WIN\033[0m \033[33m~%s\033[0m",user,home_cwd);
+    printf("\n\033[32m%s\033[0m \033[35mMINISHELL-WIN\033[0m \033[33m~%s\033[0m",user,cwd);
     printf("\n");
     printf("$ ");
 
 
     // Lire une ligne dans cmdline - Attention fgets enregistre le \n final
     if (fgets(cmdline, MAX_LINE_SIZE, stdin)==NULL) {
-      break;    //libere home_cwd
+      continue;    
     }
     cmdline[strlen(cmdline)-1]='\0';
-    if(TMPCMDLINE!=NULL){free(TMPCMDLINE);}
+    // Deallocation of allocated memory in the loop
+    if (TMPCMDLINE != NULL) {
+         free(TMPCMDLINE);
+         TMPCMDLINE = NULL; // Reset to avoid dangling pointer
+    }
     TMPCMDLINE = strdup(cmdline);
 
     //Affiche la chaine original
@@ -99,42 +85,38 @@ int main(int argc, char* argv[]) {
 
     // Traiter la ligne de commande
     //   - supprimer les espaces en début et en fin de ligne
-    trim(TMPCMDLINE);
+    if(trim(TMPCMDLINE)<=0){printf("Trim Fail");continue;}
     printf("Trim:\"%s\"\n",TMPCMDLINE);
-
+    
     //   - ajouter d'éventuels espaces autour de ; ! || && & ...
-    separate_s(TMPCMDLINE,"<&;|&\\\">2",MAX_CMD_SIZE);
-    printf("Separate:\"%s\"\n",TMPCMDLINE);
+    //if(separate_s(TMPCMDLINE,";|&\\><2$",MAX_CMD_SIZE)==0)continue;
+    //printf("Separate:\"%s\"\n",TMPCMDLINE);
 
     //   - supprimer les doublons d'espaces
-    clean(TMPCMDLINE);
+    if(clean(TMPCMDLINE)<=0){printf("Clean Fail");continue;}
     printf("clean:\"%s\"\n",TMPCMDLINE);
-
+ 
+ 
     //   - traiter les variables d'environnement
-    substenv(TMPCMDLINE,MAX_CMD_SIZE);
-
+    	if(substenv(TMPCMDLINE,MAX_CMD_SIZE)<0){
+    		printf("Substenv Fail");
+    		continue;
+    	}
+    	printf("Substenv:\"%s\"\n",TMPCMDLINE);
+    	
     // Découper la ligne dans cmdtoks
-    strcut(TMPCMDLINE,' ',cmdtoks,MAX_CMD_SIZE);
-    for (int i=0; i<MAX_CMD_SIZE; i++) {
-      if (cmdtoks[i] != NULL && cmdtoks[i] != '\0' ){
-        printf("tokens[%d]:\"%s\"\n",i, cmdtoks[i]);
-      }
-    }
-    //substenv(TMPCMDLINE,MAX_LINE_SIZE);
-    //printf("sub:\"%s\"\n",TMPCMDLINE);
-
-
-
-    /*  
-    // Découper la ligne dans cmdtoks
-    strcut(TMPCMDLINE,' ',cmdtoks,MAX_CMD_SIZE);
-    for (int i=0; i<MAX_CMD_SIZE; i++) if (cmdtoks[i] != NULL && cmdtoks[i] != '\0' )printf("tokens[%d]:\"%s\"\n",i, cmdtoks[i]);
-    */
+    if(strcut(TMPCMDLINE,' ',cmdtoks,MAX_CMD_SIZE)<=0){printf("strcut Fail");continue;}
+  	for (int i=0; i<MAX_CMD_SIZE; i++) {
+     		if (cmdtoks[i] != NULL ){
+        		printf("tokens[%d]:\"%s\"\n",i, cmdtoks[i]);
+     		}
+  	}
     
-    continue;
 
     // Traduire la ligne en structures cmd_t dans cmds
-    
+    if(parse_cmd(cmdtoks,cmds,MAX_CMD_SIZE)!=0){printf("Parse Fail");continue;}
+    current = cmds; // Commence avec la Première commande
+
     // Les commandes sont chaînées en fonction des séparateurs
     //   - next -> exécution inconditionnelle
     //   - next_success -> exécution si la commande précédente réussit
@@ -145,10 +127,38 @@ int main(int argc, char* argv[]) {
     for (current=cmds; current!=NULL; ) {
       
       // Lancer la commande
-      //exec_cmd(current);
-      
+      exec_cmd(current);
+      if (current->next_success != NULL && current->status == 0) {
+        // If the current command succeeds, move to the next_success command
+        current = current->next_success;
+    } else if (current->next_failure != NULL && current->status != 0) {
+        // If the current command fails, move to the next_failure command
+        current = current->next_failure;
+    } else {
+        // If no specific condition, move to the next command
+        current = current->next;
+    }
+
     }
   }
+  if (TMPCMDLINE != NULL) {
+        free(TMPCMDLINE);
+        TMPCMDLINE = NULL;
+    }
+
+    for (int i = 0; i < MAX_CMD_SIZE; i++) {
+        if (cmds[i].path != NULL) {
+            free(cmds[i].path);
+            cmds[i].path = NULL;
+        }
+        for (int j = 0; j < MAX_CMD_SIZE; j++) {
+            if (cmds[i].argv[j] != NULL) {
+                free(cmds[i].argv[j]);
+                cmds[i].argv[j] = NULL;
+            }
+        }
+    }
+  
   fprintf(stderr, "\nGood bye!\n");
   return 0;
 }
