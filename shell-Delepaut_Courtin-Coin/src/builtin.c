5a3e84fc71682f75ab3cd0f78a0648bec03c9cd5
ChairHomard45 <147397853+ChairHomard45@users.noreply.github.com>
2023-11-09 18:54:52 +0100

    Update builtin.c

--- shell-Delepaut_Courtin-Coin/src/builtin.c
+++ shell-Delepaut_Courtin-Coin/src/builtin.c
@@ -11,7 +11,6 @@
  
  */
 
-
 #include "cmd.h"
 #include "builtin.h"
 #include <stdlib.h>
@@ -20,7 +19,7 @@
 #include <unistd.h>
 
 int is_builtin(const char* cmd) {
-  if (strcmp(cmd, "cd") == 0 || strcmp(cmd, "exit") == 0 || strcmp(cmd, "export") == 0 || strcmp(cmd, "echo") == 0 || strcmp(cmd, "unset") == 0) {
+  if (strcmp(cmd, "cd") == 0 || strcmp(cmd, "exit") == 0 || strcmp(cmd, "export") == 0 || strcmp(cmd, "unset") == 0) {
         return 1; // Command is a built-in command
     }
     return 0; // Command is not a built-in command
@@ -33,10 +32,8 @@ int builtin(cmd_t* cmd) {
         return exit_shell(0, cmd->stout);
     }else if (strcmp(cmd->path, "export") == 0) {
         return export(cmd->argv[0],cmd->argv[1],cmd->sterr);
-    }else if (strcmp(cmd->path, "echo") == 0) {
-       return echo(cmd);
     }else if (strcmp(cmd->path, "unset") == 0) {
-       return unset(cmd->argv[0]);
+       return unset(cmd->argv[1]);
     }
     return -1; // Unrecognized command
 }
@@ -65,128 +62,12 @@ int export(const char* var, const char* value, int fderr) {
     return 0;
 }
 
-
-/**
- * @brief affiche la chaine ou le contenu du fichier ou le retour d'execution
- * 
- * @param p processus contenant les arguments nécessaires
- * @return 0 si succes, 1 sinon
- */
-int echo(cmd_t * p) {
-  FILE* output = stdout; // Default output to stdout	
-  FILE* input = stdin; // Default input from stdin
-
-  if (!strcmp(p->argv[0], "-s")) { // String input
-        if (!p->argv[1]) {
-            fprintf(stderr, "Insufficient arguments\n");
-            return 1;
-        }
-        int j = 1;
-        while (p->argv[j] != NULL) {
-            printf("%s ", p->argv[j]); // Display the string
-            j++;
-        }
-        printf("\n");
-        return 0;
-    }
-    else if (!strcmp(p->argv[0], "-f")) { // File input
-        if (!p->argv[1]) {
-            fprintf(stderr, "Insufficient arguments\n");
-            return 1;
-        }
-        char print[MAX_LINE_SIZE];
-        FILE* file = fopen(p->argv[1], "r"); // Open the file for reading
-        if (!file) {
-            fprintf(stderr, "File not found\n");
-            return 1;
-        }
-        while (fgets(print, MAX_LINE_SIZE, file)) {
-            printf("%s", print); // Display each line in the file
-        }
-        printf("\n");
-        fclose(file); // Close the file
-        return 0;
-    }else {
-        // Check for output redirection operators
-        for (int i = 1; p->argv[i] != NULL; i++) {
-            if (strcmp(p->argv[i], ">") == 0) { // Output redirection (>)
-                if (p->argv[i + 1] != NULL) {
-                    output = fopen(p->argv[i + 1], "w");
-                    if (!output) {
-                        fprintf(stderr, "Error opening file for writing\n");
-                        return 1;
-                    }
-                }
-                else {
-                    fprintf(stderr, "Insufficient arguments for output redirection\n");
-                    return 1;
-                }
-            }
-            else if (strcmp(p->argv[i], ">>") == 0) { // Output redirection (>>)
-                if (p->argv[i + 1] != NULL) {
-                    output = fopen(p->argv[i + 1], "a");
-                    if (!output) {
-                        fprintf(stderr, "Error opening file for writing\n");
-                        return 1;
-                    }
-                }
-                else {
-                    fprintf(stderr, "Insufficient arguments for output redirection\n");
-                    return 1;
-                }
-            }
-            else if (strcmp(p->argv[i], "<") == 0) { // Input redirection (<)
-                if (p->argv[i + 1] != NULL) {
-                    input = fopen(p->argv[i + 1], "r");
-                    if (!input) {
-                        fprintf(stderr, "Error opening file for reading\n");
-                        return 1;
-                    }
-                }
-                else {
-                    fprintf(stderr, "Insufficient arguments for input redirection\n");
-                    return 1;
-                }
-            }else if (strcmp(p->argv[i], "<<") == 0) { // Here-document redirection (<<)
-                if (p->argv[i + 1] != NULL) {
-                    // Read input until the delimiter
-                    char delimiter[MAX_LINE_SIZE];
-                    strcpy(delimiter, p->argv[i + 1]);
-
-                    char input_line[MAX_LINE_SIZE];
-                    while (fgets(input_line, MAX_LINE_SIZE, stdin) != NULL) {
-                        // Check if the line matches the delimiter
-                        if (strcmp(input_line, delimiter) == 0) {
-                            break; // Exit the loop if delimiter is found
-                        }
-                        fprintf(output, "%s", input_line); // Output the line
-                    }
-                }
-                else {
-                    fprintf(stderr, "Insufficient arguments for here-document redirection\n");
-                    return 1;
-                }
-            
-        }
-    }
-    }
-    if (output != stdout) {
-        fclose(output); // Close the file if output was redirected
-    }
-    
-    if (input != stdin) {
-        fclose(input); // Close the file if input was redirected
-    }
-    
-    printf("\n");
-    return 0;
-}
-
 int unset(const char* var){
 	unsetenv(var);
 }
 
+
 int exit_shell(int ret, int fderr) {
-  write(fderr,"Goodbye!",strlen("Goodbye!"));
+  write(fderr,"Goodbye!\n",strlen("Goodbye!\n"));
   exit(ret);
 }
